a36f8f167f2f848c7cf1421798f696605cc30b80
sharphand <hys83751952@gmail.com>
2021-12-28 16:36:19 +0900

    second commit

--- app.js
+++ app.js
@@ -1,47 +1,7 @@
-const calculator = {
-  add : function(a, b){
-    console.log(a + b);
-  },
-  sub: function(a, b){
-    console.log(a - b);
-  },
-  powerof: function(a, b){
-    console.log(a ** b);
-  },
-  divide: function(a, b){
-    console.log(a / b);
-  }
-};
+const title = document.querySelector(".title h1");
 
+function handleTitleClick(){
+  title.style.color = " blue";
 
-calculator.add(10, 10);
-calculator.sub(10, 5);
-calculator.powerof(10, 10);
-calculator.divide(34, 96);
-
-
-
-
-
-const calculator = {
-  add : function(a, b){
-    return(a + b);
-  },
-  sub: function(a, b){
-    return (a - b);
-  },
-  powerof: function(a, b){
-    return(a ** b);
-  },
-  divide: function(a, b){
-    return(a / b);
-  }
-};
-
-const plusResult = calculator.add(10, 10);
-
-console.log(plusResult);
-
-calculator.sub(10, 5);
-calculator.powerof(10, 10);
-calculator.divide(34, 96);
+}
+title.addEventListener("click" , handleTitleClick);
